4eebb8aacbc192858023a05a61ae5a474f28e3bf
OhhDranoel <161340592+OhhDranoel@users.noreply.github.com>
2024-08-28 17:30:43 -0300

    Adicionei uns negócio, tive q meter individual(cada pergunta)
    Adicionei a gasosa "handleAnswer"

--- questions.js
+++ questions.js
@@ -6,6 +6,13 @@ export default [
         { option: "2", correct: false },
         { option: "4", correct: false },
       ],
+      handleAnswer: function(button, correct) {
+        const isCorrect = correct;
+        document.body.style.backgroundColor = isCorrect ? 'lightgreen' : 'lightcoral';
+        button.style.backgroundColor = isCorrect ? 'lightgreen' : 'lightcoral';
+        
+        return isCorrect;
+      }
     },
     {
       question: "Quanto é 5 x 2?",
@@ -14,6 +21,13 @@ export default [
         { option: "10", correct: true },
         { option: "11", correct: false },
       ],
+      handleAnswer: function(button, correct) {
+        const isCorrect = correct;
+        document.body.style.backgroundColor = isCorrect ? 'lightgreen' : 'lightcoral';
+        button.style.backgroundColor = isCorrect ? 'lightgreen' : 'lightcoral';
+        
+        return isCorrect;
+      }
     },
     {
       question: "Quanto é 10 x 8?",
@@ -22,6 +36,13 @@ export default [
         { option: "90", correct: false },
         { option: "80", correct: true },
       ],
+      handleAnswer: function(button, correct) {
+        const isCorrect = correct;
+        document.body.style.backgroundColor = isCorrect ? 'lightgreen' : 'lightcoral';
+        button.style.backgroundColor = isCorrect ? 'lightgreen' : 'lightcoral';
+        
+        return isCorrect;
+      }
     },
     {
       question: "Quanto é 7 x 7?",
@@ -30,5 +51,12 @@ export default [
         { option: "56", correct: false },
         { option: "49", correct: true },
       ],
+      handleAnswer: function(button, correct) {
+        const isCorrect = correct;
+        document.body.style.backgroundColor = isCorrect ? 'lightgreen' : 'lightcoral';
+        button.style.backgroundColor = isCorrect ? 'lightgreen' : 'lightcoral';
+        
+        return isCorrect;
+      }
     },
   ];
